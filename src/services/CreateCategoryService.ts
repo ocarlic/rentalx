fbfde6126ba7cd35ae4adfe2fcc8deab62f0096d
Victor Carli <victorcarlibr@gmail.com>
2022-07-18 18:57:09 -0300

    feat: added principle responsibility substitution

---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,22 @@
+import { ICategoriesRepository } from "../repositories/ICategoriesRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+class CreateCategoryService {
+  constructor(private categoriesRepository: ICategoriesRepository) {}
+
+  execute({ name, description }: IRequest): void {
+    const categoryAlreadyExist = this.categoriesRepository.findByName(name);
+
+    if (categoryAlreadyExist) {
+      throw new Error("Category Already Exists!");
+    }
+
+    this.categoriesRepository.create({ name, description });
+  }
+}
+
+export { CreateCategoryService };
